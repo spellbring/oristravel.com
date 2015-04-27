fbd32999792d092bf708f4fe039a44b469893768
Jaime Reyes <jjreyes.romero88@gmail.com>
2015-04-27 10:44:56 -0400

    Se agrega un js al footer y se agrega un id a un div para ocuparlo en un metodo jquery

--- views/layout/default/_LeftSidebar.php
+++ views/layout/default/_LeftSidebar.php
@@ -916,7 +916,7 @@ $('#btnBuscarHoteles').on('click',function()
 
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div id="modal_dialog" class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
