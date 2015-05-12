43ae3fbec641a5f96155232df479a9c53bc5112d
Jaime Reyes <jjreyes.romero88@gmail.com>
2015-05-12 09:10:23 -0400

    Se agregan nuevas sesiones para el leftSideBar

--- controllers/processController.php
+++ controllers/processController.php
@@ -33,20 +33,80 @@ class processController extends Controller
         //Session::set('sess_pCR_fechaDesde', $this->getTexto('txtFechaDesde-ConsRes'));
         //Session::set('sess_pCR_fechaHasta', $this->getTexto('txtFechaHasta-ConsRes'));
         //Session::set('sess_pCR_tipoFecha', $this->getTexto('rdbFecha'));
+        Session::set('sess_pCH_ciudad',$this->getTexto('mL_cmbCiudadDestino_H'));
         
-        for($i=1; $i<=3; $i++){ 
-            Session::set('sess_BP_Adl_'.$i,$this->getTexto('mL_cmbAdultos_'.$i));          
-        }
-       // for($i=1; $i<=2; $i++){
         
-        // Session::set('sess_BP_Chd_'.$i,$this->getTexto('mL_child_'.$i));
+        for ($i = 1; $i <= 3; $i++) {
             
-        //}
+            Session::set('sess_BP_Adl_' . $i, $this->getTexto('mL_cmbAdultos_' . $i));
+                   
+        }
         
-        Session::set('sess_pBP_cntHab',$this->getTexto('ML_cmbHab'));  
+        for ($i = 1; $i <= 2; $i++) {
+            
+            Session::set('sess_BP_Chd_' . $i, $this->getTexto('mL_child_' . $i));
+                  
+        }
+
+        for($i=1; $i <= 12; $i++){
+            
+            Session::set('sess_BP_edadChd_1_'.$i, $this->getTexto('mL_edadChild_1_' . $i));
+        }
+        
+        for($i=1; $i<=12; $i++){
+            
+           Session::set('sess_BP_edadChd_2_'.$i , $this->getTexto('mL_edadChild_2_' . $i));
+            
+        }
+        for($i=0; $i<=1; $i++){
+            
+            Session::set('sess_BP_Inf_'.$i,$this->getTexto('mL_inf_'.$i));
+                    
+        }     
+        Session::set('sess_BP_noches', $this->getTexto('mL_cmbNoches_H'));
+        Session::set('sess_pBP_cntHab',$this->getTexto('ML_cmbHab'));
+        Session::set('sess_pBP_FechaIn',$this->getTexto('mL_txtFechaIn_H'));
+        Session::set('sess_pBP_FechaOut',$this->getTexto('mL_txtFechaOut_H'));
+        Session::set('sess_pBP_Cagegorias', $this->getTexto('mL_cmbCat_H'));
  
 
         $this->redireccionar('sistema/buscarHoteles');
+    }
+    
+    public function bsServ(){
+        Session::set('sess_sCH_ciudad', $this->getTexto('mL_txtCiudadDestino_S'));
+        Session::set('sess_sBP_fechaIn',$this->getTexto('mL_txtFechaIn_S'));
+
+        for($i=1; $i<=10; $i++){
+
+        Session::set('sess_sBP_serv_'.$i, $this->getTexto('mL_cmbServicios_S_'.$i));
+        }
+        
+        for($i=1; $i<=10; $i++){
+        Session::set('sess_sBP_adultos_'.$i, $this->getTexto('mL_cmbAdultos_S_'.$i));
+        }
+        
+        for($i=1; $i<=10; $i++){
+        Session::set('sess_sBP_childs_'.$i, $this->getTexto('mL_cmbChild_S_'.$i));
+        }
+        
+        $this->redireccionar('sistema/buscarServicios');
+    
+    }
+    
+    public function bsProgramas(){
+        
+       Session::set('sess_prog_Ciudad', $this->getTexto('mL_txtCiudadDestino_P'));
+       Session::set('sess_prog_FechaIn', $this->getTexto('mL_txtFechaIn_P'));
+       
+       
+       for($i=1; $i<=3; $i++){
+           
+       Session::set('sess_prog_Hab_'.$i, $this->getTexto('ML_cmbHab_P_'.$i));
+       
+       
+       } 
+        
     }
     
     public function admHoteles()
