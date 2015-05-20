0e833eb279ea9173071b628c05b9c11b06d68304
Jaime Reyes <jjreyes.romero88@gmail.com>
2015-05-20 16:13:48 -0400

    Se agregan buscarHoteles DAO y DTO para el proceso de búsquedade hoteles

---
+++ models/dao/buscarHotelesDAO.php
@@ -0,0 +1,143 @@
+<?php
+
+/* 
+ * Proyecto : oristravel.com
+ * Autor    : Tsyacom Ltda.
+ * Fecha    : Lunes, 28 de julio de 2014
+ */
+
+class buscarHotelesDAO extends Model
+{
+    public function getHoteles($idUs, $ciudad, $fechain, $fechaout, $grupo, $cantHab, $cantS, $cantD, $cantT, $cantC, $cantChild)
+    {
+        $sql= "CALL get_hoteles(".$idUs.",'".$ciudad."','".$fechain."','".$fechaout."','".$grupo."',".$cantHab.",".$cantS.",".$cantD.",".$cantT.",".$cantC.",".$cantChild.")";
+        echo $sql;
+        
+        $this->_db->consultaSP($sql);
+
+        /*if($this->_db->numRows($datos)>0)
+        {*/
+            $arrayHoteles = $this->_db->fetchAllSP();
+            $objetosHoteles = array();
+
+            foreach($arrayHoteles as $objarrHot)
+            {
+                $objHoteles = new buscarHotelesDTO();
+
+                $objHoteles->setUsuario($objarrHot['usuario']);
+                $objHoteles->setNota($objarrHot['nota']);
+                $objHoteles->setTipoh($objarrHot['tipoh']);
+                $objHoteles->setCat($objarrHot['cat']);
+                $objHoteles->setPalimt($objarrHot['palimt']);
+                $objHoteles->setPrecioFinal($objarrHot['precio_final']);
+                $objHoteles->setCompv($objarrHot['compv']);
+                $objHoteles->setComcts($objarrHot['comcts']);
+                $objHoteles->setDgcomag($objarrHot['dgcomag']);
+                $objHoteles->setCodigoHtl($objarrHot['codigo_htl']);
+
+
+
+
+
+                $objHoteles->setHotel($this->_llenarHotel(trim($objarrHot['codigo_htl'])));
+
+
+
+
+
+                $objHoteles->setCodProvee($objarrHot['cod_provee']);
+                $objHoteles->setTotal_hab($objarrHot['total_hab']);
+                $objHoteles->setTotPax($objarrHot['tot_pax']);
+                $objHoteles->setTotalNeto($objarrHot['total_neto']);
+                $objHoteles->setTotalVenta($objarrHot['total_venta']);
+                $objHoteles->setNumDias($objarrHot['num_dias']);
+                $objHoteles->setMoneda($objarrHot['moneda']);
+                $objHoteles->setStatus($objarrHot['status']);
+                $objHoteles->setTarifaIG($objarrHot['tarifa_i_g']);
+
+                $objetosHoteles[]=$objHoteles;
+            }
+         
+          return $objetosHoteles;                    
+        /*}
+        else 
+        {        
+           return false;            
+        }*/
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public function _llenarHotel($cod) {
+        $sql = 'SELECT * FROM hotel WHERE codigo = ' . $cod;
+        //echo $sql;
+        $datos = $this->_db->consulta($sql);
+        $objetosHotel = array();
+        if($this->_db->numRows($datos)>0) {
+            $arrayHotel = $this->_db->fetchAll($datos);
+            foreach($arrayHotel as $dbHotel) {
+                $objHotel = new hotelDTO();
+                $objHotel->setNombre(trim($dbHotel['hotel']));
+                $objHotel->setCiudad(trim($dbHotel['ciudad']));
+                $objHotel->setCat(trim($dbHotel['cat']));
+                $objHotel->setImgEnc(trim($dbHotel['img_encabezado']));
+                $objHotel->setDirec(trim($dbHotel['direc']));
+                //servicios de hotel
+                $objHotel->setRestaurante(trim($dbHotel['restaurante']));
+                $objHotel->setBar(trim($dbHotel['bar']));
+                $objHotel->setCafeteria(trim($dbHotel['cafeteria']));
+                $objHotel->setServHab(trim($dbHotel['s_habitacion']));
+                $objHotel->setDiscapacitados(trim($dbHotel['discapacitados']));
+                $objHotel->setLavanderia(trim($dbHotel['lavanderia']));
+                $objHotel->setBusiness(trim($dbHotel['busness_center']));
+                $objHotel->setInterHotel(trim($dbHotel['internet_hotel']));
+                $objHotel->setEstaciona(trim($dbHotel['estacionamiento']));
+                $objHotel->setBoutique(trim($dbHotel['bautique']));
+                $objHotel->setPiscinaDes(trim($dbHotel['piscina_des']));
+                $objHotel->setSpa(trim($dbHotel['spa'])); 
+                $objHotel->setGym(trim($dbHotel['gym']));
+                $objHotel->setPiscinaCub(trim($dbHotel['piscina_cub']));    
+                $objHotel->setTenis(trim($dbHotel['tenis']));
+                $objHotel->setGuarderia(trim($dbHotel['guarderia']));
+                $objHotel->setSalasReu(trim($dbHotel['salas_reunion']));
+                $objHotel->setJardin(trim($dbHotel['jardin']));
+                //servicios habitación
+                
+                $objHotel->setAcondicionado(trim($dbHotel['acondicionado']));
+                $objHotel->setCalefaccion(trim($dbHotel['calefaccion']));
+                $objHotel->setNoFuma(trim($dbHotel['no_fuma']));
+                $objHotel->setCajaFuerte(trim($dbHotel['caja_fuerte']));
+                $objHotel->setMiniBar(trim($dbHotel['mini_bar']));
+                $objHotel->setTV(trim($dbHotel['television']));
+                $objHotel->setTvCable(trim($dbHotel['tv_cable']));
+                $objHotel->setTelefono(trim($dbHotel['telefono']));
+                $objHotel->setInterHab(trim($dbHotel['inter_hab']));
+                $objHotel->setSecador(trim($dbHotel['secador_pelo']));
+                $objHotel->setBarraSeg(trim($dbHotel['barra_seguridad']));
+                
+                $objHotel->setLat(trim($dbHotel['lat']));
+                $objHotel->setLon(trim($dbHotel['lon']));
+                
+                
+                
+               
+                
+                $objetosHotel[] = $objHotel;
+            }
+            
+            //$this->_db->closeConex();
+            return $objetosHotel;
+            
+        } else {
+            return false;
+        }
+    }
+    
+}
---
+++ models/dto/buscarHotelesDTO.php
@@ -0,0 +1,221 @@
+<?php
+
+/* 
+ * Proyecto : oristravel.com
+ * Autor    : Tsyacom Ltda.
+ * Fecha    : Lunes, 28 de julio de 2014
+ */
+
+class buscarHotelesDTO{
+
+private $_usuario;
+private $_nota;
+private $_tipoh;
+private $_cat;
+private $_palimt;
+private $_precioFinal;
+private $_compv;
+private $_comcts;
+private $_dgcomag;
+private $_codigoHtl;
+private $_codProvee;
+private $_total_hab;
+private $_totPax;
+private $totalNeto;
+private $_totalVenta;
+private $_numDias;
+private $_moneda;
+private $_status;
+private $_tarifaIG;
+private $_hotel;
+
+function getHotel() {
+    return $this->_hotel;
+}
+
+function setHotel($hotel) {
+    $this->_hotel = $hotel;
+    return $this;
+}
+
+
+
+function getUsuario() {
+    return $this->_usuario;
+}
+
+function getNota() {
+    return $this->_nota;
+}
+
+function getTipoh() {
+    return $this->_tipoh;
+}
+
+function getCat() {
+    return $this->_cat;
+}
+
+function getPalimt() {
+    return $this->_palimt;
+}
+
+function getPrecioFinal() {
+    return $this->_precioFinal;
+}
+
+function getCompv() {
+    return $this->_compv;
+}
+
+function getComcts() {
+    return $this->_comcts;
+}
+
+function getDgcomag() {
+    return $this->_dgcomag;
+}
+
+function getCodigoHtl() {
+    return $this->_codigoHtl;
+}
+
+function getCodProvee() {
+    return $this->_codProvee;
+}
+
+function getTotal_hab() {
+    return $this->_total_hab;
+}
+
+function getTotPax() {
+    return $this->_totPax;
+}
+
+function getTotalNeto() {
+    return $this->totalNeto;
+}
+
+function getTotalVenta() {
+    return $this->_totalVenta;
+}
+
+function getNumDias() {
+    return $this->_numDias;
+}
+
+function getMoneda() {
+    return $this->_moneda;
+}
+
+function getStatus() {
+    return $this->_status;
+}
+
+function getTarifaIG() {
+    return $this->_tarifaIG;
+}
+
+function setUsuario($usuario) {
+    $this->_usuario = $usuario;
+    return $this;
+}
+
+function setNota($nota) {
+    $this->_nota = $nota;
+    return $this;
+}
+
+function setTipoh($tipoh) {
+    $this->_tipoh = $tipoh;
+    return $this;
+}
+
+function setCat($cat) {
+    $this->_cat = $cat;
+    return $this;
+}
+
+function setPalimt($palimt) {
+    $this->_palimt = $palimt;
+    return $this;
+}
+
+function setPrecioFinal($precioFinal) {
+    $this->_precioFinal = $precioFinal;
+    return $this;
+}
+
+function setCompv($compv) {
+    $this->_compv = $compv;
+    return $this;
+}
+
+function setComcts($comcts) {
+    $this->_comcts = $comcts;
+    return $this;
+}
+
+function setDgcomag($dgcomag) {
+    $this->_dgcomag = $dgcomag;
+    return $this;
+}
+
+function setCodigoHtl($codigoHtl) {
+    $this->_codigoHtl = $codigoHtl;
+    return $this;
+}
+
+function setCodProvee($codProvee) {
+    $this->_codProvee = $codProvee;
+    return $this;
+}
+
+function setTotal_hab($total_hab) {
+    $this->_total_hab = $total_hab;
+    return $this;
+}
+
+function setTotPax($totPax) {
+    $this->_totPax = $totPax;
+    return $this;
+}
+
+function setTotalNeto($totalNeto) {
+    $this->totalNeto = $totalNeto;
+    return $this;
+}
+
+function setTotalVenta($totalVenta) {
+    $this->_totalVenta = $totalVenta;
+    return $this;
+}
+
+function setNumDias($numDias) {
+    $this->_numDias = $numDias;
+    return $this;
+}
+
+function setMoneda($moneda) {
+    $this->_moneda = $moneda;
+    return $this;
+}
+
+function setStatus($status) {
+    $this->_status = $status;
+    return $this;
+}
+
+function setTarifaIG($tarifaIG) {
+    $this->_tarifaIG = $tarifaIG;
+    return $this;
+}
+
+
+    
+    
+    
+    
+    
+}
+
